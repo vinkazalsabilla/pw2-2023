da240840c591a3c2b1601822ccf23c5cb856f83b
vinkazalsabilla <114562947+vinkazalsabilla@users.noreply.github.com>
2023-03-15 15:50:05 +0700

    Delete pertemuan 3 directory

--- pertemuan 3/praktik1/bawah.php
+++
@@ -1,6 +0,0 @@
-<footer class="text-center py-3 bg-light mt-5">
-        <p>Created by <a href="#">Pusinfo NF &copy;2017</p>
-    </footer>
-</body>
-
-</html>
--- pertemuan 3/tugas/footer.php
+++
@@ -1,6 +0,0 @@
-<footer class="text-center py-3 bg-light mt-5">
-        <p>Created by <a href="#">Your Name</a> &copy; <?php echo date('Y') ?></p>
-    </footer>
-</body>
-
-</html>
